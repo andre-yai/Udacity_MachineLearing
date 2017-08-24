3156219735ff105e7dc53e1e21ca0f05036352c0
Andre Yai <yai.andre@gmail.com>
2017-08-24 16:57:30 -0300

    Updating ReadMe and Poi_id.py
    Code and Doc: Updating ReadMe and Poi_id.py
    
    Passing the code from notebook to the poi_id.py and also Read Me with the questions and answers.

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # Udacity_MachineLearing
+
+This is a project for the Machine Learning Module in Udacity Data Analysis Nonodegree. The aim of this project was to classify the POI in Eron Dataset. 
+
+In this project I perform some outlier removal, feature engineering, feature selection and also tested with different models,tuned the model and also did some validation and evaluation.
+
+## Context 
+
+In 2000, Enron was one of the largest companies in the United States. By 2002, it had collapsed into bankruptcy due to widespread corporate fraud. In the resulting Federal investigation, a significant amount of typically confidential information entered into the public record, including tens of thousands of emails and detailed financial data for top executives. In this project, you will play detective, and put your new skills to use by building a person of interest identifier based on financial and email data made public as a result of the Enron scandal. To assist you in your detective work, we've combined this data with a hand-generated list of persons of interest in the fraud case, which means individuals who were indicted, reached a settlement or plea deal with the government, or testified in exchange for prosecution immunity.
+
+
+## To run 
+
+It should run the script poi_id.py and then tester.py to evaluate our model. This project was done with python2.7 so you may also need to use conda in order to run this project.
+
+### Questions 
+
+1. Summarize for us the goal of this project and how machine learning is useful in trying to accomplish it. As part of your answer, give some background on the dataset and how it can be used to answer the project question. Were there any outliers in the data when you got it, and how did you handle those? [relevant rubric items: “data exploration”, “outlier investigation”]
+
+	The goal of this project is to succesfully identify the POIs of the Eron scandal. In order to do it I will use machine learning classify algorithm, that can throught the data identify patterns of the POI. In this project I will use the dataset contaning some financial data and email data.
+
+2. What features did you end up using in your POI identifier, and what selection process did you use to pick them? Did you have to do any scaling? Why or why not? As part of the assignment, you should attempt to engineer your own feature that does not come ready-made in the dataset -- explain what feature you tried to make, and the rationale behind it. (You do not necessarily have to use it in the final analysis, only engineer and test it.) In your feature selection step, if you used an algorithm like a decision tree, please also give the feature importances of the features that you use, and if you used an automated feature selection function like SelectKBest, please report the feature scores and reasons for your choice of parameter values. [relevant rubric items: “create new features”, “intelligently select features”, “properly scale features”]
+
+	I end up using in my POI identifier the features 'poi', 'salary', 'restricted_stock', 'bonus', 'total_payments', 'total_stock_value' to select them I used the SelectKBest and I end up latter scalling them in oder to to standarlize the features. I created two new variables fraction of emails received from POI and fraction of emails sent to poi. I noticed that the features that helped me to identify poi were the financial features.
+
+
+3. What algorithm did you end up using? What other one(s) did you try? How did model performance differ between algorithms? [relevant rubric item: “pick an algorithm”]
+
+	I tried to use knn, naive bayes, decision trees and some ensembles classifiers like adaboost and random forest. I noticed that with knn. I had a better accuracy so I picked as my final algorithm.
+
+
+4. What does it mean to tune the parameters of an algorithm, and what can happen if you don’t do this well? How did you tune the parameters of your particular algorithm? What parameters did you tune? (Some algorithms do not have parameters that you need to tune -- if this is the case for the one you picked, identify and briefly explain how you would have done it for the model that was not your final choice or a different model that does utilize parameter tuning, e.g. a decision tree classifier). [relevant rubric items: “discuss parameter tuning”, “tune the algorithm”]
+
+
+	Tuning the algorithm means finding the best parameters for a certain model, that gives a better results. I tuned my particular algorithm with GridSearchCV. For my Adaboost model I tuned the n_estimators. And the final result was
+
+
+5. What is validation, and what’s a classic mistake you can make if you do it wrong? How did you validate your analysis? [relevant rubric items: “discuss validation”, “validation strategy”]
+
+	Validation process is when you divided your dataset into train, test datasets. A classic mistake is to not divide into these to dataset and use the entire data to train your model. I validade my model using a croess-validation (that means dividing my dataset into small chucks and using most of them to train and a chuck to test and then repeat the process).To validate my model I used a  k-fold = 5.
+
+6. Give at least 2 evaluation metrics and your average performance for each of them. Explain an interpretation of your metrics that says something human-understandable about your algorithm’s performance. [relevant rubric item: “usage of evaluation metrics”]
+
+	And also to validate I used the accuracy, reacall and prediction socer. Accuracy is the metric for the pecentage of correctness of the model.
+	In my final model I got Accuracy Score: 100% Recall Score: 100% Precision Score: 100%.
+
--- final_project/poi_id.py
+++ final_project/poi_id.py
@@ -6,6 +6,7 @@ sys.path.append("../tools/")
 
 from feature_format import featureFormat, targetFeatureSplit
 from tester import dump_classifier_and_data
+from __future__ import division
 
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
@@ -16,14 +17,54 @@ features_list = ['poi','salary'] # You will need to use more features
 with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
 
+
 ### Task 2: Remove outliers
 ### Task 3: Create new feature(s)
 ### Store to my_dataset for easy export below.
 my_dataset = data_dict
+del data_dict["TOTAL"]
 
+
+def compute_fraction_poi_total_messages(poi_messages, total_messages):
+    fraction_messages = 0.
+    
+    if(poi_messages !='NaN' and total_messages != 'NaN'):
+        fraction_messages = poi_messages/total_messages
+    
+    return fraction_messages
+
+def compute_fraction_from_poi_to_person(person_data):
+
+    qtd_messages_total = person_data['to_messages']
+    qtd_messages_from_poi = person_data['from_poi_to_this_person']
+    fraction_messages_from_poi = compute_fraction_poi_total_messages(qtd_messages_from_poi,qtd_messages_total)
+    
+    return fraction_messages_from_poi
+
+def compute_fraction_from_person_to_poi(person_data):
+    
+    qtd_messages_to_total = person_data['from_messages']
+    qtd_messages_to_poi = person_data['from_this_person_to_poi']
+    fraction_messages_to_poi = compute_fraction_poi_total_messages( qtd_messages_to_poi, qtd_messages_to_total)
+    
+    return fraction_messages_to_poi
+
+
+feature_list = data_dict.values()[0].keys()
+for person_name in my_dataset:
+    person_data = my_dataset[person_name]
+    person_data["fraction_messages_from_poi"] = compute_fraction_from_poi_to_person(person_data);
+    person_data["fraction_messages_to_poi"] = compute_fraction_from_person_to_poi(person_data);
+    print(person_data["fraction_messages_from_poi"],person_data["fraction_messages_to_poi"])
 ### Extract features and labels from dataset for local testing
 data = featureFormat(my_dataset, features_list, sort_keys = True)
 labels, features = targetFeatureSplit(data)
+
+new_features = ["fraction_messages_from_poi","fraction_messages_to_poi"]
+
+for feature_name in new_features:
+    if feature_name  not in features_list:
+        features_list.append(feature_name)
 
 ### Task 4: Try a varity of classifiers
 ### Please name your classifier clf for easy export below.
@@ -31,9 +72,70 @@ labels, features = targetFeatureSplit(data)
 ### you'll need to use Pipelines. For more info:
 ### http://scikit-learn.org/stable/modules/pipeline.html
 
-# Provided to give you a starting point. Try a variety of classifiers.
+my_data = featureFormat(my_dataset, features_list, sort_keys = True)
+labels, features = targetFeatureSplit(my_data)
+
+# Train and test
+from sklearn.model_selection import train_test_split
+features_train, features_test, labels_train, labels_test = \
+    train_test_split(features, labels, test_size=0.3, random_state=42)
+
+#Function that will help to compute metrics.
+from sklearn.metrics import accuracy_score,precision_score,recall_score
+
+def compute_metrics(predict,labels_test):
+
+    accuracy = accuracy_score(predict,labels_test);
+    recall = recall_score(predict,labels_test);
+    precision = precision_score(predict,labels_test);
+
+    print "Accuracy Score: {}%".format(accuracy) + " Recall Score: {}%".format(recall) + " Precision Score: {}%".format(precision);
+
+# Feature selection - selectionkbest, select percentile,lasso regression
+from sklearn.feature_selection import SelectPercentile,SelectKBest,chi2
+
+selector = SelectKBest(k=10)
+new_data = selector.fit_transform(features,labels)
+scores_KB = selector.scores_
+
+print("Final shape of parameters",new_data.shape)
+print("Feature scores:",selector.scores_)
+                     
+# Provided to give you a starting point. Try a variety of classifiers.                            
 from sklearn.naive_bayes import GaussianNB
 clf = GaussianNB()
+clf.fit(features_train,labels_train)
+predict = clf.predict(features_test)
+compute_metrics(predict,labels_test);
+
+from sklearn.neighbors import KNeighborsClassifier
+clf = KNeighborsClassifier(n_neighbors=3)
+clf.fit(features_train,labels_train)
+predict = clf.predict(features_test)
+compute_metrics(predict,labels_test);
+
+
+#Decision Tree
+from sklearn.tree import DecisionTreeClassifier
+
+clf = DecisionTreeClassifier()
+clf.fit(features_train,labels_train)
+predict = clf.predict(features_test)
+compute_metrics(predict,labels_test);
+
+
+from sklearn.ensemble import AdaBoostClassifier
+clf = AdaBoostClassifier(n_estimators=110)
+clf.fit(features_train,labels_train)
+predict = clf.predict(features_test)
+compute_metrics(predict,labels_test);
+
+from sklearn.ensemble import RandomForestClassifier
+clf = RandomForestClassifier(n_estimators=100)
+clf.fit(features_train,labels_train)
+predict = clf.predict(features_test)
+compute_metrics(predict,labels_test);
+
 
 ### Task 5: Tune your classifier to achieve better than .3 precision and recall 
 ### using our testing script. Check the tester.py script in the final project
@@ -42,11 +144,54 @@ clf = GaussianNB()
 ### stratified shuffle split cross validation. For more info: 
 ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
-# Example starting point. Try investigating other evaluation techniques!
-from sklearn.model_selection import train_test_split
-features_train, features_test, labels_train, labels_test = \
-    train_test_split(features, labels, test_size=0.3, random_state=42)
 
+from sklearn.model_selection import StratifiedKFold,GridSearchCV
+
+def init_cross_validation(train_reduced, labels):
+    
+    cross_validation = StratifiedKFold(n_splits=3)
+    cross_validation.get_n_splits(train_reduced, labels)
+    
+    return cross_validation;
+
+def generate_grid_search_model(model,parameters,train_reduced,labels):
+    
+    cross_validation = init_cross_validation(train_reduced, labels);
+    
+    grid_search = GridSearchCV(model,
+                               scoring='accuracy',
+                               param_grid = parameters,
+                               cv = cross_validation)
+
+    grid_search.fit(train_reduced, labels)
+    model = grid_search
+    parameters = grid_search.best_params_
+
+    print('Best score: {}'.format(grid_search.best_score_))
+    print('Best parameters: {}'.format(grid_search.best_params_))
+    
+    return model
+
+def create_AdaBoost(train_reduced,labels,run_grid_search=False):
+    
+    if run_grid_search:
+        
+        parameter_grid = {
+            'n_estimators': [100,110,120,130]
+        }
+        clf = AdaBoostClassifier()
+        clf = generate_grid_search_model(clf,parameter_grid,train_reduced,labels)
+    else: 
+        parameters = {
+            'n_estimators': 130
+        }
+
+        clf = AdaBoostClassifier(**parameters)
+        clf.fit(train_reduced, labels)
+
+    return clf
+    
+clf = create_AdaBoost(features,labels,run_grid_search=True)
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
 ### that the version of poi_id.py that you submit can be run on its own and
