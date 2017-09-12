38f47003dc6885f936239e55c9b7bdcbe31a6905
Andre Yai <yai.andre@gmail.com>
2017-09-12 15:54:58 -0300

    Updating ReadMe
    Title: Updating ReadMe
    
    I updated ReadMe to better answer the questions.

--- final_project/tester.py
+++ final_project/tester.py
@@ -41,7 +41,7 @@ def test_classifier(clf, dataset, feature_list, folds = 1000):
         for jj in test_idx:
             features_test.append( features[jj] )
             labels_test.append( labels[jj] )
-        
+
         ### fit the classifier using training set, and test on test set
         clf.fit(features_train, labels_train)
         predictions = clf.predict(features_test)
